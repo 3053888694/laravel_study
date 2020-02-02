d2b5b2b7be370938583c5688f7da1a2059443aa4
vagrant <vagrant@homestead>
2020-02-02 12:41:28 +0000

    添加授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,16 @@ use App\User;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => [
+                'show'
+            ]
+        ]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -15,11 +25,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, User $user, ImageUploadHandler $imageUploadHandler)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
         if ($request->avatar) {
             $result = $imageUploadHandler->save($request->avatar, 'avatars', $user->id, 416);
